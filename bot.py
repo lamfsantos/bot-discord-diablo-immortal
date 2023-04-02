d3fb256f9da27594215d9809b7c73388d3019b7c
Luiz Andre Furlan <lamfsantos5@gmail.com>
2023-04-02 17:31:59 -0400

    scheduler pra rodar função async com 1 minuto de intervalo

--- bot.py
+++ bot.py
@@ -1,20 +1,45 @@
 import nextcord
+import datetime
 from nextcord.ext import commands
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import asyncio
 
-TESTING_GUILD_ID = 123456789  # Replace with your guild ID
+GUILD_ID = 1089708881363599530  # Replace with your guild ID
+TOKEN = "MTA4OTY3MzcwNzk5MzE5MDUyMg.GiUMqL.pyomkUoe8HYUDjltYeDOH6QjyX_gAyHUJ2aUWc" # Put your bot token here
 
 bot = commands.Bot()
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+	print(f'We have logged in as {bot.user}')
 
-    channel = bot.get_channel(TESTING_GUILD_ID)
+	channel = bot.get_channel(GUILD_ID)
 
-    await channel.send("Mensagem")
+	#await channel.send("Mensagem")
+	#await notify_event(channel);
+	await teste()
 
-@bot.slash_command(description="My first slash command", guild_ids=[TESTING_GUILD_ID])
-async def hello(interaction: nextcord.Interaction):
-    await interaction.send("Teste")
+async def teste():
+	channel = bot.get_channel(GUILD_ID)
+	await channel.send("Mensagem")
 
-bot.run('token here')
+async def notify_event(channel):
+	time_now = datetime.datetime.now()
+	
+	print(str(time_now.hour + 4 == 18))
+
+	if time_now.hour + 3 == 18:
+		await channel.send("Evento")
+
+# @bot.slash_command(description="My first slash command", guild_ids=[TESTING_GUILD_ID])
+# async def hello(interaction: nextcord.Interaction):
+#     await interaction.send("Teste")
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(teste, 'interval', minutes=1)
+scheduler.start()
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
+
+    asyncio.get_event_loop().run_forever()
